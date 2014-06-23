7938ac9fbdbcd2bcce6b2cef7c6d1443934794e8
Eduardo Quagliato <eduardo@quagliato.me>
2014-06-23 08:57:53 -0300

    Adding custom files for dashboard and admin's section on dashboard.

--- form/dashboard.php
+++ form/dashboard.php
@@ -6,15 +6,10 @@
 ?>
 
                 <h1>Painel</h1>
+                <?php include_once("custom/custom_dashboard.php"); ?>
                 <?php if($usuario->get('privilegio') == 'ADM') : ?>
                 <h2>Admin</h2>
-                <ul>
-                    <li><a href="<?=APP_URL?>/admin/info_panel">Info Panel</a></li>
-                    <li><a href="<?=APP_URL?>/admin/valor/listar">Valores</a></li>
-                    <li><a href="<?=APP_URL?>/admin/blacklist/listar">Blacklist</a></li>
-                    <!--li><a href="<?=APP_URL?>/admin/isencao/cadastrar">Cadastrar Isenção</a></li-->
-                    <li><a href="<?=APP_URL?>/admin/inscricao/buscar">Buscar Inscrição</a></li>
-                </ul>
+                <?php include_once("custom/custom_dashboard_admin.php"); ?>
                 <?php endif; ?>
 
 <?php Structure::footer(); ?>
