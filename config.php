6680fec7dd7db77f4da1796839ea6bb9be01f54d
Eduardo Quagliato <eduardo@quagliato.me>
2014-06-23 09:00:07 -0300

    Removing my own credentials from config file.

--- config.php
+++ config.php
@@ -3,19 +3,19 @@
     define('APP_URL', 'http://localhost/noisy-cricket');
     define('APP_TITLE', 'noisy-cricket');
 
-    define('ADMIN_EMAIL', 'eduardo@quagliato.me');
+    define('ADMIN_EMAIL', '');
 
     define('FILES_URL', APP_URL.'/uploads/');
     define('FILES_URL_FAILSAFE', APP_URL.'/uploads/failsafe/');
     //define('FILES_DIR', APP_DIR.'/uploads/');
-    define('FILES_DIR', '/home/quagliato/Dropbox/Sandbox/noisy-cricket/uploads/');
-    define('FILES_DIR_FAILSAFE', '/home/quagliato/Dropbox/Sandbox/n_goiania/uploads/failsafe/');
+    define('FILES_DIR', '');
+    define('FILES_DIR_FAILSAFE', '');
     define('MAX_FILESIZE', '50MB');
 
-    define('DB_HOST', 'localhost');
-    define('DB_USER', 'root');
-    define('DB_PASS', '9u4gl14t0');
-    define('DB_NAME', 'noisy-cricket');
+    define('DB_HOST', '');
+    define('DB_USER', '');
+    define('DB_PASS', '');
+    define('DB_NAME', '');
 
     define('SQL_LOG_FILENAME', 'sql.log');
 
