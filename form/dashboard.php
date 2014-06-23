49f17db961c007e2ab96e4eb76d4fe813254f385
Eduardo Quagliato <eduardo@quagliato.me>
2014-06-23 12:47:47 -0300

    Removing useless objects on dashboard file.

--- form/dashboard.php
+++ form/dashboard.php
@@ -1,14 +1,11 @@
 <?php
     $usuario = Structure::verifySession();	
 	Structure::header();
-
-    $usu_id = $usuario->get('id');
 ?>
 
                 <h1>Painel</h1>
                 <?php include_once("custom/custom_dashboard.php"); ?>
                 <?php if($usuario->get('privilegio') == 'ADM') : ?>
-                <h2>Admin</h2>
                 <?php include_once("custom/custom_dashboard_admin.php"); ?>
                 <?php endif; ?>
 
