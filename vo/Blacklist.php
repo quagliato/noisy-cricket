68eeffa2a2c0ff74bb2dce35b819203d536a3f11
Eduardo Quagliato <eduardo@quagliato.me>
2014-06-30 03:32:10 -0300

    Fixing GenericClass.php include on app.php.

--- vo/Blacklist.php
+++ vo/Blacklist.php
@@ -1,39 +1,16 @@
 <?php
 
-class Blacklist {
+class Blacklist extends GenericClass{
 
-	const sys_tablename = "blacklist";
+	protected static $sys_tablename = "blacklist";
 	
-	private $id;
-	private $user_email;
+	protected $id;
+	protected $user_email;
 
-	private $sys_type = array(
+	protected $sys_type = array(
 	    'id' => 'int',
 	    'user_email' => 'str'
     );
-	
-	public function set($prop, $value) {
-		$this->$prop = $value;
-	}
-	
-	public function get($prop) {
-		return $this->$prop;
-	}
-	
-	public function type($prop) {
-		if (array_key_exists($prop, $this->sys_type))
-			return $this->sys_type[$prop];
-		return 'none';
-	}
-	
-	public function props() {
-		return get_object_vars($this);
-	}
-	
-	public static function tablename() {
-		return self::sys_tablename;
-	}
-	
 }
 
 ?>
