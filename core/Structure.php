af9f2bfdf8e2458e3ffafab2a26726df62c5f68e
Eduardo Quagliato <eduardo@quagliato.me>
2014-08-21 14:38:18 -0300

    Adding support for custem header.php and footer.php files.

--- core/Structure.php
+++ core/Structure.php
@@ -1,17 +1,17 @@
 <?php
 
 class Structure {
-	
-	public static function redir($page) {
-		$page = APP_URL.$page;
-		echo "<meta HTTP-EQUIV='Refresh' CONTENT='0;URL=$page' />";
-		exit;
-	}
-	
-	public static function redirWithMessage($message, $page) {
-		echo "<script>alert('".$message."');</script>";
-		Structure::redir($page);
-	}
+    
+    public static function redir($page) {
+        $page = APP_URL.$page;
+        echo "<meta HTTP-EQUIV='Refresh' CONTENT='0;URL=$page' />";
+        exit;
+    }
+    
+    public static function redirWithMessage($message, $page) {
+        echo "<script>alert('".$message."');</script>";
+        Structure::redir($page);
+    }
 
     public static function dashboardLink() {
         $text = NULL;
@@ -21,41 +21,49 @@ class Structure {
         
         return "<a href=\"".APP_URL."/dashboard\">$text</a>";
     }
-	
-	public static function verifySession() {
-		session_start();
+    
+    public static function verifySession() {
+        session_start();
 
-		if (!isset($_SESSION['user_id'])) {
-			Structure::redirWithMessage("Usuario nao logado.", '/'); //TODO: Adicionar acento
-		} else {
-			$usuario_dao = new UsuarioDAO;
-			$usuario = $usuario_dao->getUserById($_SESSION['user_id']);
-			if (!$usuario) {
-				Structure::redirWithMessage("Usuario nao encontrado no sistema.","/"); //TODO: Adicionar acento
-			} else {
+        if (!isset($_SESSION['user_id'])) {
+            Structure::redirWithMessage("Usuario nao logado.", '/'); //TODO: Adicionar acento
+        } else {
+            $usuario_dao = new UsuarioDAO;
+            $usuario = $usuario_dao->getUserById($_SESSION['user_id']);
+            if (!$usuario) {
+                Structure::redirWithMessage("Usuario nao encontrado no sistema.","/"); //TODO: Adicionar acento
+            } else {
                 return $usuario;
             }
-		}
-	}
+        }
+    }
 
     public static function verifyAdminSession() {
-	    $usuario = Structure::verifySession();
+        $usuario = Structure::verifySession();
 
         if ($usuario->get('privilegio') == 'ADM') {
             return $usuario;
         }
 
         Structure::redirWithMessage("Area restrita apenas para administradores.", "/"); //TODO: Adicionar acento
-	}
+    }
 
-	
-	public static function header() {
-		include_once('theme/header.php');
-	}
-	
-	public static function footer() {
-		include_once('theme/footer.php');
-	}
+    
+    public static function header() {
+        include_once("theme/header.php");
+    }
+
+    public static function customHeader($header) {
+        include_once("theme/header-$header.php");
+    }
+
+    public static function footer() {
+        include_once("theme/footer.php");
+    }
+
+    public static function customFooter($footer) {
+        include_once("theme/footer-$footer.php");
+    }
 
 }
 
