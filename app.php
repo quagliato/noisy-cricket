4f915f6c0504623b31918663d17ab390e4a3bb5c
Eduardo Quagliato <eduardo@quagliato.me>
2015-02-08 12:47:32 -0200

    Reorganizing files.

--- app.php
+++ app.php
@@ -1,24 +1,23 @@
 <?php
     include_once("core/DBStuff.php");
     include_once("core/Filler.php");
+    include_once("core/LogEngine.php");
+    include_once("core/Notification.php");
     include_once("core/StrGenerator.php");
     include_once("core/Structure.php");
     include_once("core/DataBinder.php");
     include_once("core/TxtFile.php");
-    include_once("core/LogEngine.php");
+    include_once("core/Utils.php");
+    include_once("core/Validation.php");
 
-    include_once("vo/GenericClass.php");
-    include_once("vo/Blacklist.php");
-    include_once("vo/Request.php");
-    include_once("vo/Usuario.php");
+    include_once("model/GenericClass.php");
+    include_once("model/Blacklist.php");
+    include_once("model/Request.php");
+    include_once("model/Usuario.php");
     
     include_once("dao/GenericDAO.php");
     include_once("dao/BlacklistDAO.php");
     include_once("dao/UsuarioDAO.php");
-
-    include_once("classes/Notification.php");
-    include_once("classes/Validation.php");
-    include_once("classes/Utils.php");
 
     include_once("custom/custom_app.php");
 ?>
