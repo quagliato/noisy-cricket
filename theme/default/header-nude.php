60e82a94fee4cbb0cae2cc8f9e2266abb424cb6a
Eduardo Quagliato <eduardo@quagliato.me>
2015-06-28 11:18:06 -0300

    Fixing font-awesome import.

--- theme/default/header-nude.php
+++ theme/default/header-nude.php
@@ -21,7 +21,7 @@
         <script type="text/javascript" src="<?=APP_URL?>/theme/<?=THEME?>/_libs/jquery.mask.min.js"></script>
         <script type="text/javascript" src="<?=APP_URL?>/theme/<?=THEME?>/_libs/jquery-ui-1.11.2/jquery-ui.min.js"></script>
         <link rel="stylesheet" type="text/css" href="<?=APP_URL?>/theme/<?=THEME?>/_libs/jquery-ui-1.11.2/jquery-ui.min.css" />
-        <link rel="stylesheet" type="text/css" href="<?=APP_URL?>/theme/<?=THEME?>/_libs/font-awesome-4.3.0/font-awesome.min.css" />
+        <link rel="stylesheet" type="text/css" href="<?=APP_URL?>/theme/<?=THEME?>/_libs/font-awesome-4.3.0/css/font-awesome.min.css" />
         <!-- /Third-party libs -->
 
         <script>
